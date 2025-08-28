f888bebbfed12d5383153cbca68b69d38c9ecf00
Jason <jason.brigou@hotmail.com>
2025-08-28 11:50:27 +0200

    End of section 7

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,9 +1,10 @@
 import { AppBar, Badge, Box, IconButton, LinearProgress, List, ListItem, Toolbar, Typography } from "@mui/material";
 import { DarkMode, LightMode, ShoppingCart } from "@mui/icons-material";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useAppSelector } from "../store/store";
 import { useDispatch } from "react-redux";
 import { setDarkMode } from "./uiSlice";
+import { useFetchBasketQuery } from "../../features/basket/basketApi";
 
 const midLinks = [
     { title: 'catalog', path: '/catalog' },
@@ -31,6 +32,9 @@ const navStyles = {
 export default function NavBar() {
     const { isLoading, darkMode } = useAppSelector(state => state.ui)
     const dispatch = useDispatch();
+    const { data: basket } = useFetchBasketQuery();
+
+    const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0) || 0;
 
     return (
         <AppBar position="fixed">
@@ -56,8 +60,8 @@ export default function NavBar() {
                 </List>
 
                 <Box display='flex' alignItems='center'>
-                    <IconButton size="large" sx={{ color: 'inherit' }}>
-                        <Badge badgeContent='4' color="secondary">
+                    <IconButton component={Link} to="/basket" size="large" sx={{ color: 'inherit' }}>
+                        <Badge badgeContent={itemCount} color="secondary">
                             <ShoppingCart />
                         </Badge>
                     </IconButton>
