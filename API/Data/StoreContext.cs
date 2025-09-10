23d69cc3c9b1789bc2f8cfadc6dfaf4168bc34e4
Jason <jason.brigou@hotmail.com>
2025-09-10 15:16:17 +0200

    End of section 9

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,11 +1,24 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class StoreContext(DbContextOptions options) : DbContext(options)
+public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
     public required DbSet<Product> Products { get; set; }
     public required DbSet<Basket> Baskets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityRole>()
+            .HasData(
+                new IdentityRole { Id = "ce3b7062-e8ca-45b0-b0b4-5c475d47f2c5", Name = "Member", NormalizedName = "MEMBER" },
+                new IdentityRole { Id = "ac92a1fa-e7b2-484f-9e28-7dcbaae07191", Name = "Admin", NormalizedName = "ADMIN" }
+            );
+    }
 }
 
--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -5,6 +5,8 @@ import { useAppSelector } from "../store/store";
 import { useDispatch } from "react-redux";
 import { setDarkMode } from "./uiSlice";
 import { useFetchBasketQuery } from "../../features/basket/basketApi";
+import UserMenu from "./UserMenu";
+import { useUserInfoQuery } from "../../features/account/accountApi";
 
 const midLinks = [
     { title: 'catalog', path: '/catalog' },
@@ -30,6 +32,7 @@ const navStyles = {
 }
 
 export default function NavBar() {
+    const {data: user} = useUserInfoQuery();
     const { isLoading, darkMode } = useAppSelector(state => state.ui)
     const dispatch = useDispatch();
     const { data: basket } = useFetchBasketQuery();
@@ -66,18 +69,22 @@ export default function NavBar() {
                         </Badge>
                     </IconButton>
 
-                    <List sx={{ display: 'flex' }}>
-                        {rightLinks.map(({ title, path }) => (
-                            <ListItem
-                                component={NavLink}
-                                to={path}
-                                key={path}
-                                sx={navStyles}
-                            >
-                                {title.toUpperCase()}
-                            </ListItem>
-                        ))}
-                    </List>
+                    {user ? (
+                        <UserMenu user={user} />
+                    ) : (
+                        <List sx={{ display: 'flex' }}>
+                            {rightLinks.map(({ title, path }) => (
+                                <ListItem
+                                    component={NavLink}
+                                    to={path}
+                                    key={path}
+                                    sx={navStyles}
+                                >
+                                    {title.toUpperCase()}
+                                </ListItem>
+                            ))}
+                        </List>
+                    )}
                 </Box>
 
             </Toolbar>
