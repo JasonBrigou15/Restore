3f80b1185198efd53bee9a95e1a313aece11b811
Jason <jason.brigou@hotmail.com>
2025-09-18 19:38:38 +0200

    Inventory complete

--- client/src/app/layout/UserMenu.tsx
+++ client/src/app/layout/UserMenu.tsx
@@ -1,7 +1,7 @@
 import { Button, Menu, Fade, MenuItem, ListItemIcon, ListItemText, Divider } from "@mui/material";
 import { useState } from "react";
 import type { User } from "../models/user";
-import { Person, History, Logout } from "@mui/icons-material";
+import { Person, History, Logout, Inventory } from "@mui/icons-material";
 import { useLogoutMutation } from "../../features/account/accountApi";
 import { Link } from "react-router-dom";
 
@@ -26,7 +26,7 @@ export default function UserMenu({ user }: Props) {
                 onClick={handleClick}
                 color='inherit'
                 size="large"
-                sx={{fontSize: '1.1rem'}}
+                sx={{ fontSize: '1.1rem' }}
             >
                 {user.email}
             </Button>
@@ -54,6 +54,13 @@ export default function UserMenu({ user }: Props) {
                     </ListItemIcon>
                     <ListItemText>My orders</ListItemText>
                 </MenuItem>
+                {user.roles.includes('Admin') &&
+                    <MenuItem component={Link} to='/inventory'>
+                        <ListItemIcon>
+                            <Inventory />
+                        </ListItemIcon>
+                        <ListItemText>Inventory</ListItemText>
+                    </MenuItem>}
                 <Divider />
                 <MenuItem onClick={logout}>
                     <ListItemIcon>
